a8f690e6ceef459fdea835adc70ea79164cb901c
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-05-23 16:45:51 -0500

    add notes about middleware

--- src/server.js
+++ src/server.js
@@ -5,33 +5,30 @@ import cors from 'cors'
 
 export const app = express()
 
-app.disable('x-powered-by')
+app.disable('x-powered-by') // https://github.com/expressjs/discussions/issues/39
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+// these are middleware to run functions to transform the data before the controller gets or posts or...
+// middleware does not respond to req, it just transforms it and sends it on to the next one
+// ok, they aren't designed to respond to requests like controllers, but they can if they need to/you make them
+app.use(cors()) // makes the server CORS enabled
+app.use(json()) // transforms the request into easily digested JSON. The node way w/o express is hard
+app.use(urlencoded({ extended: true })) //allows us to attach params to url query string
+app.use(morgan('dev')) // does logging when server starts up
 
-app.get('/', (req, res) => {
-  res.send({ message: 'hello' })
+// let's make out own middleware
+
+// this is a controller!
+// the final fn that runs before respone. between request and response
+app.get('/data', (req, res) => {
+  res.send({ message: 'hello again' })
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send({ message: 'ok' })
-})
-
-app.get('/cats', (req, res) => {
-  res.send({ cat: 'Kirby' })
-})
-
-app.post('/cats', (req, res) => {
-  console.log(req.body)
-  res.send({ message: 'hello' })
+app.post('/data', (req, res) => {
+  res.send(req.body)
 })
 
 export const start = () => {
   app.listen(2000, () => {
-    console.log('server is on port 2000')
+    console.log('server running on port 2000')
   })
 }
