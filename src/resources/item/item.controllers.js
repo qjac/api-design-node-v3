84e7a2bd5cfec1683b52eb09bb571952e61524b5
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-05-24 12:50:31 -0500

    my answer

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,4 @@
-export default {}
+import { crudControllers } from '../../utils/crud'
+import { Items } from './items.model'
+
+export default crudControllers(Items)
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,19 @@
 import { Router } from 'express'
+import controllers from './item.controllers'
 
 const router = Router()
+
+// /api/items
+router
+  .route('/')
+  .get(controllers.getOne)
+  .post(controllers.createOne)
+
+// /api/items/:id
+router
+  .route('/:id')
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.removeOne)
 
 export default router
