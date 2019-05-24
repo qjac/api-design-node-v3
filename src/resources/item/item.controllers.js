d7d97a7ebe092ad4166c0ab3cbfff9e7bbee0585
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-05-24 16:17:20 -0500

    add notes

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,17 @@
-export default {}
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:28017/api-test')
+  const item = await Item.create({
+    name: 'clean up',
+    createdBy: mongoose.SchemaTypes.ObjectId,
+    list: mongoose.SchemaTypes.ObjectId
+  })
+
+  console.log(item)
+}
+
+// export default {}
+run()
