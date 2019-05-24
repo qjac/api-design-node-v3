a01fc7571b47453c217ed57e4413e232a831c2ac
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-05-24 15:53:09 -0500

    after walkthrough

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -16,9 +16,15 @@ const itemSchema = new mongoose.Schema(
     },
     notes: String,
     due: Date,
-    createdBy: { ref: 'user', required: true, type: Object },
-    list: { ref: 'list', required: true, type: Array }
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: { type: mongoose.SchemaTypes.ObjectId, ref: 'list', required: true }
   },
   { timestamps: true }
 )
+
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
